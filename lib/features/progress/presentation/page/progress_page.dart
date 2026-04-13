d1405ca424c75277bf6b5c30094e6841847b3ef9
polina <polinasemushevaa@gmail.com>
2026-04-13 15:57:33 +0500

    feat:add progress tracking page

---
+++ lib/features/progress/presentation/page/progress_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProgressPage extends StatelessWidget {
+  const ProgressPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        surfaceTintColor: Colors.transparent,
+        title: Text(
+          'Your Progress',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        toolbarHeight: 46,
+      ),
+      body: Center(child: Text("Will be soon...")),
+    );
+  }
+}
