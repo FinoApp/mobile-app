8f3e72ff7c1af0c48641aced16187a041a985cd1
polina <polinasemushevaa@gmail.com>
2026-04-29 14:34:29 +0500

    refactor: extract base URL into api_constants

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,1 @@
+const String kBaseUrl = 'http://3.80.90.99/api/v1';
--- lib/core/providers/dio_provider.dart
+++ lib/core/providers/dio_provider.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:financial_ccounting/core/constants/api_constants.dart';
 import 'package:financial_ccounting/core/providers/token_storage_provider.dart';
 import 'package:financial_ccounting/core/services/auth_interceptor.dart';
 
@@ -7,7 +8,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final dioProvider = Provider<Dio>((ref) {
   final dio = Dio(
     BaseOptions(
-      baseUrl: 'http://3.80.90.99/api/v1',
+      baseUrl: kBaseUrl,
       connectTimeout: Duration(seconds: 30),
       receiveTimeout: Duration(seconds: 30),
       sendTimeout: Duration(seconds: 30),
